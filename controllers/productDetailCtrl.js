65741c8f068f910e325569d6cc7895f3cedf9cc1
David Lars Ketch <davidlarsketch@gmail.com>
2018-04-19 11:04:43 -0500

    Inits product details view-ctrl pair

---
+++ controllers/productDetailCtrl.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports.displayProductDetail = (req, res, next) => {
+  //Gets details for a product
+  //Renders product-details.pug
+};
+
+module.exports.addToCart = (req, res, next) => {
+  //Checks whether user is logged in
+  //Redirects to login, if not?
+  //Adds product to authed user's cart
+  //Renders a notification that the product was successfully added
+};
