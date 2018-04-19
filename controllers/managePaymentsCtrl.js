0f3802bb90a6eb2b0fe4c203a2a652f10b34ca21
David Lars Ketch <davidlarsketch@gmail.com>
2018-04-19 11:02:05 -0500

    Inits manage payments ctrl-view pair

---
+++ controllers/managePaymentsCtrl.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports.displayPaymentOptions = (req, res, next) => {
+  // Gets authed user's payment options
+  // Renders manage-payments.pug
+};
+
+module.exports.displayAddNewPaymentOption = (req, res, next) => {
+  // Renders new-payment-option.pug
+};
+
+module.exports.addNewPaymentOption = (req, res, next) => {
+  // Posts validated form from new-payment-option.pug
+  // Re-directs to manage-payments.pug
+};
+
+module.exports.removePaymentOption = (req, res, next) => {
+  // Deletes payment option from user's account
+  // Re-renders manage-payments.pug?
+  // Or does a client.js fn remove that payment option from the DOM?
+};
