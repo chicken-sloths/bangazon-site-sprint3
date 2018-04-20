7a048f41ac41107adb2dd44a989240fcb99cef21
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-04-20 11:32:47 -0500

    changed route to reflect mels pr

--- app/routes/productsRouter.js
+++ app/routes/productsRouter.js
@@ -6,9 +6,9 @@ const productsRouter = Router();
 const { addNewProductForSale, renderAddProductForm } = require('../controllers/manageProductsCtrl');
 
 // render the form to add a new product
-productsRouter.get('/new', renderAddProductForm);
+productsRouter.get('/manage/new', renderAddProductForm);
 
 // when the user clicks 'submit' on the form to add a new product
-productsRouter.post('/new', addNewProductForSale);
+productsRouter.post('/manage/new', addNewProductForSale);
 
 module.exports = productsRouter;
