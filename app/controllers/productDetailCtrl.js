04994ff715339212fb782bcaa695b2b61702fad8
David Lars Ketch <davidlarsketch@gmail.com>
2018-04-20 13:00:28 -0500

    Fixes #5: user can see product details, add to cart if currently logged in

--- app/controllers/productDetailCtrl.js
+++ app/controllers/productDetailCtrl.js
@@ -1,13 +1,53 @@
 'use strict';
 
 module.exports.displayProductDetail = (req, res, next) => {
-  //Gets details for a product
-  //Renders product-details.pug
+  const { Product, ProductOrder } = req.app.get('models');
+  Product.find({
+    where: {id: req.params.id},
+    include: [
+      { model: ProductOrder, group: 'product_id' }
+    ]
+  })
+  .then(({dataValues}) => {
+    dataValues.quantity -= dataValues.ProductOrders.length
+    dataValues.query = req.query;
+    res.render('product-details', dataValues);
+  })
+  .catch(err => next(err));
 };
 
 module.exports.addToCart = (req, res, next) => {
-  //Checks whether user is logged in
-  //Redirects to login, if not?
-  //Adds product to authed user's cart
-  //Renders a notification that the product was successfully added
+  const product_id = req.params.id;
+  const { Product, ProductOrder, Order } = req.app.get('models');
+
+  if (!req.isAuthenticated()) {
+    // Currently redirects to login, but we might talk about a better solution
+    return res.redirect('/login');
+  }
+  Order.find({
+    where: {
+      customer_id: req.user.id,
+      payment_option_id: null
+    }
+  })
+  .then(({dataValues: {id}}) => {
+    const order_id = id;
+
+    Product.findById(product_id)
+    .then(({dataValues: {current_price}}) => {
+      const price = current_price;
+
+      return ProductOrder.create({
+        order_id,
+        price,
+        product_id
+      });
+    })
+    .then(resp => {
+      // Renders the page again b/c quantity needs to change
+      // ?added=true is accessible at req.query, used in pug template for
+      // success message
+      res.redirect(`/product/${product_id}?added=true`);
+    });
+  });
 };
