f4dd68118830cab89749868e1c50de2f8ddbc1e2
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-04-20 11:45:19 -0500

    search route working

--- app/controllers/searchCtrl.js
+++ app/controllers/searchCtrl.js
@@ -1,6 +1,18 @@
 'use strict';
 
-module.exports.displaySearchResults = (req, res, next) => {
-  // Gets all products matching search query
-  // Renders search.pug
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
+
+module.exports.searchProductsByName = (req, res, next) => {
+  const { Product } = req.app.get('models');
+  Product.findAll({
+    where: {
+      title: { [Op.iLike]: '%'+req.params.term+'%' }
+    },
+    limit: 10,
+    raw: true
+  })
+    .then(products => {
+      res.render('search', { term: req.params.term, products });
+    });
 };
