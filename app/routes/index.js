9e9184bd2ecec024a447e5a3fdc7c9de04e6ddda
David Lars Ketch <davidlarsketch@gmail.com>
2018-04-20 12:49:03 -0500

    Provides routes for product-detail

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,11 +4,18 @@ const { Router } = require('express');
 const router = Router();
 const { getProductsByType } = require('../controllers/productTypesCtrl');
 
+const {
+  displayProductDetail,
+  addToCart
+} = require('../controllers/productDetailCtrl');
+
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
 router.get('/categories/:id', getProductsByType);
+router.get('/product/:id', displayProductDetail);
+router.post('/product/:id', addToCart);
 
 // pipe all other requests through the route modules
 router.use(require('./authRoute'));
