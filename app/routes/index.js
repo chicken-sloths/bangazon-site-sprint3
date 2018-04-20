80b2fb080ea3762451a800176c0c7a07f58a394c
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-04-20 13:40:00 -0500

    catches user id

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,6 +5,12 @@ const router = Router();
 const { getProductsByType } = require('../controllers/productTypesCtrl');
 const { searchProductsByName } = require('../controllers/searchCtrl');
 
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/login');
+}
+
 router.get('/', (req, res, next) => {
   res.render('index');
 });
@@ -14,6 +20,8 @@ router.post('/search', searchProductsByName);
 
 // pipe all other requests through the route modules
 router.use(require('./authRoute'));
+router.use(isLoggedIn);
+
 // router.use(require('./foo'));
 
 // require in all the products routes
