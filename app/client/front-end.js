c7c7f02a05c616a24b3e6fa64003db1e52a04f21
Melissa Bell <melissabell456@gmail.com>
2018-04-22 14:27:30 -0500

    product id gathered as result of button click and passed as req param for DELETE roue!

--- app/client/front-end.js
+++ app/client/front-end.js
@@ -1,33 +1,13 @@
 'use strict';
 
-console.log($(".deleteProd"));
-
-// const deleteProdBtn = document.getElementsByClassName('deleteProd');
-// deleteProdBtn.addEventListener('click', () => {
-
-// });
-
-$('#staticDiv').on('click', 'yourSelector', function() {
-  //do something
-});
-$(window).on('click', $(".deleteProd"), () => {
-  console.log("hello");
+$(window).on('click', $(".deleteProd"), (event) => {
+  let productId = event.target.id;
   $.ajax({
-    url: 'http://localhost:8080/products/manage/2',
+    url: `http://localhost:8080/products/manage/${productId}`,
     type: 'DELETE',
     return: 'false',
     success: function(result) {
         console.log("deleted");
     }
+  });
 });
-})
-// $(".deleteProd").click(() => {
-//   console.log("hello");
-//   $.ajax({
-//     url: '/manage/:id',
-//     type: 'DELETE',
-//     success: function(result) {
-//         console.log("deleted");
-//     }
-// });
-// })
