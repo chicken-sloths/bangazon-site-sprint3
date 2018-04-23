116117690e27c28e91bfb9e6b45521dc56a56ae3
David Lars Ketch <davidlarsketch@gmail.com>
2018-04-23 13:47:57 -0500

    Refactors front-end.js for deleting from cart; better targetting of IDs/classes

--- app/client/front-end.js
+++ app/client/front-end.js
@@ -1,23 +1,34 @@
 'use strict';
 
-$(".deleteProdBtn").on('click', (event) => {
-  let productId = event.target.id;
+$("#deleteProdBtn").on('click', (event) => {
+  let productId = event.target.dataset.id;
   $.ajax({
     url: `/products/manage/remove/${productId}`,
     type: 'PATCH',
-    success: function(result) {
-        $(`#productCard${productId}`).remove();
+    success: result => {
+        $(`.product[data-id=${productId}]`).remove();
     }
   });
 });
 
-$(".deletePayOpBtn").on('click', (event) => {
+$("#deletePayOpBtn").on('click', (event) => {
   let payOpId = event.target.dataset.id;
   $.ajax({
     url: `/payment/${payOpId}`,
     type: 'DELETE',
     success: result => {
         $(`.payment-option[data-id=${payOpId}]`).remove();
+    }
+  });
+});
+
+$("#deleteFromCart").on('click', (event) => {
+  let productId = event.target.dataset.id;
+  $.ajax({
+    url: `/cart/remove/${productId}`,
+    type: 'DELETE',
+    success: result => {
+        $(`.prodcut[data-id=${productId}]`).remove();
     }
   });
 });
