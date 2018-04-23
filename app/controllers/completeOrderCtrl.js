ba0edeadb49feed43167d786f2774b7209f0207f
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-04-23 11:39:15 -0500

    patches payment option data but might patch twice

--- app/controllers/completeOrderCtrl.js
+++ app/controllers/completeOrderCtrl.js
@@ -10,10 +10,22 @@ module.exports.displayCheckoutForm = (req, res, next) => {
     }
   })
     .then(paymentOpts => {
-      console.log('paymentopts', paymentOpts);
       res.render('complete-order', { paymentOpts });
     })
     .catch(err => {
       next(err);
     })
 };
+
+module.exports.closeOrder = (req, res, next) => {
+  console.log('CLOSE ORDER FUNCTION CALLED!');
+  const { Order } = req.app.get('models');
+  const newData = {
+    payment_option_id: req.body.payment_option_id
+  };
+
+  Order.update(newData, { where: { customer_id: req.user.id } })
+    .then(updatedOrder => {
+      console.log(updatedOrder)
+    })
+}
