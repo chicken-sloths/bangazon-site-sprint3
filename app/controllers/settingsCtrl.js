d900d28407a21196598e4a31817ef9cba7e135a7
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-04-24 10:12:34 -0500

    built out edit customer info function in ctrl

--- app/controllers/settingsCtrl.js
+++ app/controllers/settingsCtrl.js
@@ -9,3 +9,16 @@ module.exports.displayUsersSettings = (req, res, next) => {
   })
   .catch(err => res.status(404));
 };
+
+
+module.exports.editUserSettings = (req, res, next) => {
+  const { Customer } = req.app.get('models');
+  const newData = {
+    street_address: req.body.street_address,
+    city: req.body.city,
+    state: req.body.state,
+    postal_code: req.body.postal_code,
+    phone_number: req.body.phone_number,
+  }
+
+}
