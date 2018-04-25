3144aaf46b7a59a1d54637bfd7f3e280f30125ee
David Lars Ketch <davidlarsketch@gmail.com>
2018-04-25 11:29:07 -0500

    Defines deleteRecommendation fn in controller

--- app/controllers/recommendationCtrl.js
+++ app/controllers/recommendationCtrl.js
@@ -8,7 +8,20 @@ module.exports.displayRecommendations = (req, res, next) => {
     return customer.getRecommendations()
   })
   .then(recommendations => {
-    res.render('recommendations', {products: recommendations});
+    res.render('recommendations', {products: recommendations, state: "recommendations"});
+  })
+  .catch(err => next(err));
+};
+
+module.exports.deleteRecommendation = (req, res, next) => {
+  const { Customer } = req.app.get('models');
+
+  Customer.findById(req.user.id)
+  .then(customer => {
+    return customer.removeRecommendation(req.params.id)
+  })
+  .then(resp => {
+    res.status(200).json(resp);
   })
   .catch(err => next(err));
 };
